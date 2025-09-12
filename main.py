37ccc4342316659e4520cc978cbc15867fb7c1ad
Sevaguila <sveragui@umag.cl>
2025-09-12 05:31:39 -0300

    Primer commit del proyecto

---
+++ main.py
@@ -0,0 +1,16 @@
+from rich.console import Console
+from rich.table import Table
+
+
+
+def main():
+
+    console = Console()
+
+    # Texto con estilo
+    console.print("[bold red]¡Error![/bold red] Ocurrió un problema.")
+
+
+
+if __name__ == "__main__":
+    main()
