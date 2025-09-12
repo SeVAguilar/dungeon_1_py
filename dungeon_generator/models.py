50891d244f283a0208f285c604b567ca053b606e
Sevaguila <sveragui@umag.cl>
2025-09-12 05:53:14 -0300

    Agrego contenido al script models.py

---
+++ dungeon_generator/models.py
@@ -0,0 +1,26 @@
+
+
+class Habitacion:
+    pass
+
+class Mapa:
+    pass
+
+class Objeto:
+    pass
+
+class Explorador:
+    pass
+
+class ContenidoHabitacion:
+    pass
+
+class Tesoro(ContenidoHabitacion):
+    pass
+
+class Monstro(ContenidoHabitacion):
+    pass
+
+class Jefe(Monstro):
+    pass
+
